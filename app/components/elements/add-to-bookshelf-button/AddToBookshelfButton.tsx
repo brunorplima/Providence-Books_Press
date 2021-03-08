08112b334cf50301d70f16d467798ff84697d85a
Bruno Lima <brunolima.cad@gmail.com>
2021-03-08 02:36:22 -0700

    new component for handling add to bookshelf feature

---
+++ app/components/elements/add-to-bookshelf-button/AddToBookshelfButton.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Frame from '../../layouts/Frame'
+import Button from '../button/Button'
+
+interface Props {
+   clickHandler: Function
+}
+
+const AddToBookshelfButton: React.FC<Props> = ({ clickHandler }) => {
+
+   const frameStyle = {
+      padding: '.5rem',
+      display: 'flex',
+      justifyContent: 'center'
+   }
+
+   return (
+      <Frame style={frameStyle}>
+         <Button label='ADD TO BOOKSHELF' clickHandler={clickHandler} />
+      </Frame>
+   )
+}
+
+export default AddToBookshelfButton
