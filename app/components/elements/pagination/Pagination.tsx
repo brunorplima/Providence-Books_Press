b01a55e800a9ac360cc93c115cd2a1c846b4e4c1
Bruno Lima <brunolima.cad@gmail.com>
2021-03-08 02:44:09 -0700

    new component for pagination handling

---
+++ app/components/elements/pagination/Pagination.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Pagination = () => {
+   return (
+      <div style={{display: 'flex'}}>
+         <div>1</div>
+         <div>2</div>
+         <div>3</div>
+         <div>...</div>
+         <div>8</div>
+      </div>
+   )
+}
+
+export default Pagination
