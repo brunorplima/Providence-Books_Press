b791605a0f0ab6f133d94f2c67205f0cc7e808a5
Bruno Lima <brunolima.cad@gmail.com>
2021-03-11 23:24:33 -0700

    new component: to handle checkbox click on sidebar

---
+++ app/components/elements/checkbox-input/CheckboxInput.tsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react'
+
+interface Props {
+   id: string,
+   option: string,
+   optionsChecked: string[],
+   setCheckedList: (options: string[]) => void
+}
+
+const CheckboxInput: React.FC<Props> = ({ id, option, optionsChecked, setCheckedList }) => {
+
+   const [checked, setChecked] = useState<boolean>(false);
+
+   useEffect(() => {
+      if (optionsChecked.includes(option)) {
+         setChecked(true)
+      }
+   }, []);
+
+   useEffect(() => {
+      if (checked) {
+         const newOptionsChecked = [...optionsChecked]
+         newOptionsChecked.push(option);
+         setCheckedList(newOptionsChecked);
+      } else {
+         const newOptionsChecked = optionsChecked.filter(opt => opt !== option);
+         setCheckedList(newOptionsChecked)
+      }
+   }, [checked]);
+
+   return (
+      <input
+         type='checkbox'
+         id={id}
+         defaultChecked={checked}
+         onChange={() => setChecked(!checked)}
+      />
+   )
+}
+
+export default CheckboxInput
