25d2a6660721ea4b194248fcf43f8ca41c97b60f
Bruno Lima <brunolima.cad@gmail.com>
2021-03-11 23:28:17 -0700

    FilterBox receives and passes down setCheckedList func

--- app/components/elements/filter-box/FilterBox.tsx
+++ app/components/elements/filter-box/FilterBox.tsx
@@ -1,21 +1,29 @@
 import React from 'react'
+import CheckboxInput from '../checkbox-input/CheckboxInput'
 
 interface Props {
    boxTitle: string,
    optionsList: string[],
-   optionsChecked: string[]
+   optionsChecked: string[],
+   setCheckedList: (value: string[]) => void 
 }
 
-const FilterBox: React.FC<Props> = ({ boxTitle, optionsList, optionsChecked }) => {
+const FilterBox: React.FC<Props> = ({ boxTitle, optionsList, optionsChecked, setCheckedList }) => {
+
    return (
       <div>
          <h4>{boxTitle}</h4>
          {
-            optionsList.map((option => {
+            optionsList.map(((option, idx) => {
                return (
                   <div key={option}>
-                     <input type='checkbox' name='name' defaultChecked={optionsChecked.includes(option)} />
-                     <label htmlFor='name'>{option}</label>
+                     <CheckboxInput
+                        id={boxTitle + idx}
+                        option={option}
+                        optionsChecked={optionsChecked}
+                        setCheckedList={setCheckedList}
+                     />
+                     <label htmlFor={boxTitle + idx}>{option.toUpperCase()}</label>
                   </div>
                )
             }))
