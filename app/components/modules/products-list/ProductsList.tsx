6d1506212562c311f84c84f9dccbf24d83263b85
Bruno Lima <brunolima.cad@gmail.com>
2021-03-15 07:12:39 -0600

    changed to FC and it renders a horizontal list

--- app/components/modules/products-list/ProductsList.tsx
+++ app/components/modules/products-list/ProductsList.tsx
@@ -1,50 +1,64 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Product from '../../../interfaces-objects/Product';
-import Book from '../../../interfaces-objects/Book';
-import ProductItem from '../../elements/product-item/ProductItem'
+import HorizontalProductsList from './HorizontalProductsList'
 import Frame from '../../layouts/Frame'
+import useScreenWidth from '../../../util/useScreenWidth'
 
 interface Props {
-   products: Product[]
+   products: Product[],
+   nonPaginatedListLength: number
 }
 
-export class ProductsList extends Component<Props> {
+const ProductsList: React.FC<Props> = ({ products, nonPaginatedListLength }) => {
 
+   const screenWidth = useScreenWidth();
 
-   render() {
-      const { products } = this.props;
-
-      const frameStyle = {
-         padding: 10,
-         display: 'flex',
-         flexWrap: 'wrap'
-      }
-
-      const outerFrame = {
-         padding: 10,
-      }
-      return (
-         <Frame style={{}}>
-            <Frame style={frameStyle}>
-               {
-                  products.map((product, i) => {
-                     return (
-                        <ProductItem
-                           key={product._id}
-                           productId={product._id}
-                           images={product.images}
-                           authors={(product as Book).authors}
-                           name={product.name}
-                           price={product.price}
-                           addToBookshelf={() => { }}
-                        />
-                     )
-                  })
-               }
-            </Frame>
-         </Frame>
-      )
+   const frameStyle = {
+      padding: 10,
+      display: 'flex',
+      flexWrap: 'wrap'
    }
+
+   function numberItemsHorizontalList() {
+      if (screenWidth <= 450) return 1;
+      if (screenWidth <= 650) return 2;
+      if (screenWidth <= 768) return 3;
+      if (screenWidth <= 850) return 2;
+      if (screenWidth <= 1050) return 3;
+      if (screenWidth <= 1180) return 4;
+      return 5;
+   }
+
+   function getHorizontalLists() {
+      const eachLength = numberItemsHorizontalList();
+      const splitted: Product[][] = []
+      let idx = 0;
+      while (idx < products.length) {
+         if (idx + eachLength < products.length)
+            splitted.push(products.slice(idx, idx + eachLength))
+         else
+            splitted.push(products.slice(idx, products.length))
+
+         idx += eachLength;
+      }
+
+      return splitted;
+   }
+
+   const horizontalLists = getHorizontalLists();
+
+   return (
+      <div className='products-list'>
+         <Frame style={frameStyle}>
+            <div>Showing {products.length} out of {nonPaginatedListLength}</div>
+         </Frame>
+         <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+            {
+               horizontalLists.map(list => <HorizontalProductsList productItems={list} />)
+            }
+         </div>
+      </div>
+   )
 }
 
 export default ProductsList
