fd801c84f01d2985b33b1dd20238a586478b5a16
Bruno Lima <brunolima.cad@gmail.com>
2021-03-15 07:09:40 -0600

    added css module and changed some props

--- app/components/elements/product-item/ProductItem.tsx
+++ app/components/elements/product-item/ProductItem.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
-import Frame from '../../layouts/Frame'
 import Link from 'next/link'
-import Head from 'next/head'
 import AddToBookshelfButton from '../add-to-bookshelf-button/AddToBookshelfButton'
 import ProductItemFlag from './ProductItemFlag'
+import styles from '../../../styles/products-list/ProductItem.module.css'
 
 interface Props {
    productId: string,
-   image?: string,
-   author: string,
+   images?: string[],
+   authors: string | null,
    name: string,
    price: number,
    addToBookshelf: Function
@@ -16,34 +15,24 @@ interface Props {
 
 const ProductItem: React.FC<Props> = ({
    productId,
-   image,
-   author,
+   images,
+   authors,
    name,
    price,
    addToBookshelf
 }) => {
 
-   const frameStyle = {
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'space-between',
-      padding: '.2rem',
-      margin: 5,
-      width: 560,
-      flex: 1
-   }
-
    return (
-      <Frame style={{...frameStyle, width: 700}}>
+      <div className={styles.container}>
          <div style={{}}>
             <Link href='#'>
                <a>
                   {/* {price > 40 && price < 70 && <ProductItemFlag flag='NEW' />} */}
                   <img 
-                     src='https://elyssarpress.com/wp-content/uploads/2019/12/book-cover-placeholder.png'
-                     style={{maxWidth: '148px'}}
+                     src={images[0]}
+                     style={{maxWidth: '165px'}}
                      />
-                  <p>{author}</p>
+                  <p>{authors}</p>
                   <h2>{name}</h2>
                   <p>$ {price.toFixed(2)}</p>
                </a>
@@ -53,7 +42,7 @@ const ProductItem: React.FC<Props> = ({
          <div>
             <AddToBookshelfButton clickHandler={() => addToBookshelf(productId)} />
          </div>
-      </Frame>
+      </div>
    )
 }
 
