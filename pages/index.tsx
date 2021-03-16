b81a861d685e4b792fb1a02c53be1d542fb1b376
Bruno Lima <brunolima.cad@gmail.com>
2021-03-16 02:23:29 -0600

    code refactor

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,14 @@ import styles from '../app/styles/Home.module.css'
 
 export default function Home() {
    return (
-      <div className={styles.container}>
+      <>
          <Head>
             <title>Providence B&amp;P</title>
             <link rel="icon" href="/favicon.ico" />
          </Head>
-         
-      </div>
+         <div className={styles.container}>
+
+         </div>
+      </>
    )
 }
