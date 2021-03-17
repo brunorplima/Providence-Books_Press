865f57487bf14b01ee26a35be32c79112959b722
Bruno Lima <brunolima.cad@gmail.com>
2021-03-16 23:11:17 -0600

    added structure and style to product flag

--- app/components/elements/product-item/ProductItemFlag.tsx
+++ app/components/elements/product-item/ProductItemFlag.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
+import styles from '../../../styles/products-list/ProductItem.module.css'
 
 interface Props {
    flag: string
 }
 
 const ProductItemFlag: React.FC<Props> = ({ flag }) => {
+   if (!flag) return null;
+
    return (
-      <div>
+      <div className={styles.flag}>
          <div>{flag}</div>
+         <div className={styles.triangleTopLeft}></div>
+         <div className={styles.triangleBottomLeft}></div>
       </div>
    )
 }
