8b96b64b60669769093c36417e9feebc02bc16c7
Bruno Lima <brunolima.cad@gmail.com>
2021-03-22 05:06:47 -0600

    added new boolean prop to apply new styles

--- app/components/elements/product-item/ProductItemFlag.tsx
+++ app/components/elements/product-item/ProductItemFlag.tsx
@@ -2,17 +2,18 @@ import React from 'react'
 import styles from '../../../styles/products-list/ProductItem.module.css'
 
 interface Props {
-   flag: string
+   flag: string,
+   isRelativePos?: boolean
 }
 
-const ProductItemFlag: React.FC<Props> = ({ flag }) => {
+const ProductItemFlag: React.FC<Props> = ({ flag, isRelativePos }) => {
    if (!flag) return null;
 
    return (
-      <div className={styles.flag}>
+      <div className={(!isRelativePos ? styles.defaultFlag : styles.biggerFlag) + ' ' + styles.flag}>
          <div>{flag}</div>
-         <div className={styles.triangleTopLeft}></div>
-         <div className={styles.triangleBottomLeft}></div>
+         <div className={isRelativePos ? `${styles.triangleTopLeftDefault} ${styles.triangleTopLeft}` : styles.triangleTopLeftDefault}></div>
+         <div className={isRelativePos ? `${styles.triangleBottomLeftDefault} ${styles.triangleBottomLeft}` : styles.triangleBottomLeftDefault}></div>
       </div>
    )
 }
