a080e5174be5eec07cb8faef850d7041a8f46f6e
Bruno Lima <brunolima.cad@gmail.com>
2021-03-25 13:00:39 -0600

    added message in case there is no providence review

--- app/components/modules/product-details/ProvidenceReview.tsx
+++ app/components/modules/product-details/ProvidenceReview.tsx
@@ -5,12 +5,17 @@ interface Props {
    providenceReview: string
 }
 
+const noReviewMessage = `We have unfortunately not had the oppurtunity yet to review this book.\nDue to the authors previous literature and thought we still at this time feel free to sell this book.\nLike any other book we ask that you would be like the Bereans and read with the scriptures open.\nDiscerning whether what is written is from God.`;
+
 const ProvidenceReview: React.FC<Props> = ({ providenceReview }) => {
    return (
       <div className={styles.container}>
          <h3>PROVIDENCE BOOKS REVIEW</h3>
          {
+            providenceReview ?
             providenceReview.split('\n').map(paragraph => <p key={paragraph}>{paragraph}</p>)
+            :
+            noReviewMessage.split('\n').map(paragraph => <p key={paragraph}>{paragraph}</p>)
          }
       </div>
    )
