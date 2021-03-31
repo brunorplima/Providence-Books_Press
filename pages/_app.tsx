e7997ce3cdfa818394d20751af0457ef22b43b53
Bruno Lima <brunolima.cad@gmail.com>
2021-03-31 03:13:52 -0600

    adapted app to use redux; added layout above app level

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,21 @@
+import { Provider } from 'react-redux'
+import { store, persistor } from '../app/redux/store/store'
+import { PersistGate } from 'redux-persist/integration/react'
+
+
 import '../app/styles/globals.css'
+import Layout from '../app/components/layouts/Layout'
 
 function MyApp({ Component, pageProps }) {
-   return <Component {...pageProps} />
+   return (
+      <Provider store={store}>
+         <PersistGate persistor={persistor}>
+            <Layout>
+               <Component {...pageProps} />
+            </Layout>
+         </PersistGate>
+      </Provider>
+   )
 }
 
 export default MyApp
