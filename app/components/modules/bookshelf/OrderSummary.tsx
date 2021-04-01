0ce0480ed70547d52b134e5b1215c8c84b9ade16
Bruno Lima <brunolima.cad@gmail.com>
2021-04-01 02:15:52 -0600

    substituted dummy data to receive props

--- app/components/modules/bookshelf/OrderSummary.tsx
+++ app/components/modules/bookshelf/OrderSummary.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import styles from '../../../styles/bookshelf/OrderSummary.module.css'
 
 interface Props {
-
+   subtotal: number,
+   shippingFee: number,
+   gst: number,
 }
 
-const OrderSummary: React.FC<Props> = ({ }) => {
+const OrderSummary: React.FC<Props> = ({ subtotal, shippingFee, gst }) => {
    return (
       <div className={styles.container}>
          <h2 className={styles.heading}>Order Summary</h2>
@@ -13,26 +15,26 @@ const OrderSummary: React.FC<Props> = ({ }) => {
          <div className={styles.summary}>
             <div className={styles.summaryBetween}>
                <div>SUBTOTAL</div>
-               <div>$ 514.11</div>
+               <div>$ {subtotal.toFixed(2)}</div>
             </div>
 
             <div className={styles.summaryBetween}>
                <div>SHIPPING FEE </div>
-               <div>$ 18.00</div>
+               <div>$ {shippingFee.toFixed(2)}</div>
             </div>
 
             <div className={styles.summaryEnd}>
-               <div>$ 532.11</div>
+               <div>$ {(subtotal + shippingFee).toFixed(2)}</div>
             </div>
 
             <div className={styles.summaryBetween}>
                <div>GST</div>
-               <div>$ 26.61</div>
+               <div>$ {gst.toFixed(2)}</div>
             </div>
 
             <div className={`${styles.summaryBetween} ${styles.summaryTotal}`}>
                <div>TOTAL</div>
-               <div>$ 558.72</div>
+               <div>$ {(subtotal + shippingFee + gst).toFixed(2)}</div>
             </div>
          </div>
       </div>
