eb768198b0ca0c1546cfefd5dd9d8aa68ad16307
Bruno Lima <brunolima.cad@gmail.com>
2021-04-01 00:34:18 -0600

    added increase and decrease quantity functions

--- app/components/elements/bookshelf-item-qty/BookshelfItemQuantity.tsx
+++ app/components/elements/bookshelf-item-qty/BookshelfItemQuantity.tsx
@@ -3,17 +3,31 @@ import styles from '../../../styles/elements/BookshelfItemQuantity.module.css'
 import { AiFillMinusCircle, AiFillPlusCircle } from 'react-icons/ai'
 
 interface Props {
+   itemId: string,
    quantity: number,
-   setQuantity: (quantity: number) => void,
+   increaseQuantity: (id: string) => void,
+   decreaseQuantity: (id: string) => void,
    evenItem?: boolean
 }
 
-const BookshelfItemQuantity: React.FC<Props> = ({ quantity, setQuantity, evenItem }) => {
+const BookshelfItemQuantity: React.FC<Props> = ({ itemId, quantity, increaseQuantity, decreaseQuantity, evenItem }) => {
    return (
       <div className={styles.container}>
-         <div className={`${styles.controller} ${evenItem && `${styles.lightColor}`}`}><AiFillPlusCircle /></div>
+         <div 
+            className={`${styles.controller} ${evenItem && `${styles.lightColor}`}`}
+            onClick={() => increaseQuantity(itemId)}
+         >
+            <AiFillPlusCircle />
+         </div>
+         
          <div className={`${styles.quantity} ${evenItem && `${styles.lightColor} ${styles.lightBorder}`}`}>{quantity}</div>
-         <div className={`${styles.controller} ${evenItem && `${styles.lightColor}`}`}><AiFillMinusCircle /></div>
+         
+         <div 
+            className={`${styles.controller} ${evenItem && `${styles.lightColor}`}`}
+            onClick={() => decreaseQuantity(itemId)}   
+         >
+            <AiFillMinusCircle />
+         </div>
       </div>
    )
 }
