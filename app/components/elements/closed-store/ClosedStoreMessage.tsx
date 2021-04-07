0530c86c7d76b5a7aa2a74423c68611f59b3c5df
Bruno Lima <brunolima.cad@gmail.com>
2021-04-07 01:46:22 -0600

    changed style

--- app/components/elements/closed-store/ClosedStoreMessage.tsx
+++ app/components/elements/closed-store/ClosedStoreMessage.tsx
@@ -5,30 +5,34 @@ const ClosedStoreMessage = () => {
 
    const date = new Date(Date.now());
 
-   if (date.getDay() !== 0 && date.getDay() !== 6) return null;
+   if (date.getDay() !== 0) return null;
 
    return (
       <div className={styles.container}>
-         <div className={styles.message}>
-            <div className={styles.closedSignContainer}>
-               <div className={styles.pin}></div>
-               <div className={styles.leftCord}></div>
-               <div className={styles.rightCord}></div>
-               <div className={styles.closedSign}>
-                  <div>
-                     <div className={styles.sorry}>Sorry</div>
-                     <div className={styles.were}>WE'RE</div>
-                     <div className={styles.closed}>CLOSED</div>
+         <div className={styles.messageContainer}>
+            <div className={styles.message}>
+               <div className={styles.closedSignContainer}>
+                  
+                  <div className={styles.pin}></div>
+                  <div className={styles.leftCord}></div>
+                  <div className={styles.rightCord}></div>
+                  <div className={styles.closedSign}>
+                     <div>
+                        <div className={styles.sorry}>Sorry</div>
+                        <div className={styles.were}>WE'RE</div>
+                        <div className={styles.closed}>CLOSED</div>
+                     </div>
                   </div>
                </div>
-            </div>
 
-            <div>
-               <div>Sorry, the store is closed on Sundays.</div>
-               <div>Please come back tomorrow. Thank you!</div>
-            </div>
+               <div>
+                  <div>Sorry, the store is closed on Sundays.</div>
+                  <div>Please come back tomorrow. Thank you!</div>
+               </div>
 
-            <img src='/full-logo-min.png' alt='' />
+               <img src='/full-logo-min.png' alt='' />
+
+            </div>
          </div>
       </div>
    )
