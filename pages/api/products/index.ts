8e6ffd82caf6267c591213d32ce446836fc9cfa2
Bruno Lima <brunolima.cad@gmail.com>
2021-05-11 11:06:52 -0600

    added line for future use

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import Product from '../../../app/interfaces-objects/Product';
 import productsJSON from './products.json'
+// import products from './dataCreator'
 
 export default async (req: NextApiRequest, res: NextApiResponse<Product[]>) => {
    const products = productsJSON as Product[]
