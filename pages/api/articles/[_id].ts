d1ee41ee7ddaa3300852087aeac5378472419083
Bruno Lima <brunolima.cad@gmail.com>
2021-05-11 11:05:19 -0600

    added type for the return value of the api for articles

--- pages/api/articles/[_id].ts
+++ pages/api/articles/[_id].ts
@@ -1,8 +1,15 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import articles from './articles.json'
+import { Article } from '../../../app/interfaces-objects/interfaces';
+import articlesJSON from './articles.json'
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
+type NotFound = { error: string }
+
+export default (req: NextApiRequest, res: NextApiResponse<Article | NotFound>) => {
    const { _id } = req.query;
-   const article = articles.find((art) => art._id === _id);
+
+   let articles = articlesJSON as unknown as Article[];
+
+   const article: Article = articles.find((art: Article) => art._id === _id);
+
    res.status(200).json(article);
 }
