969af59789327426626c39a75610ca0e0fbc0eca
Bruno Lima <brunolima.cad@gmail.com>
2021-05-12 15:29:32 -0600

    Increased button's width

--- pages/articles/index.tsx
+++ pages/articles/index.tsx
@@ -56,7 +56,7 @@ export class index extends Component<Props, State> {
                {
                   !this.state.showCategories &&
                   <div style={{ marginBottom: '5rem', display: 'flex', justifyContent: 'center' }}>
-                     <Button clickHandler={this.setShowCategories} label='Show by categories' style={{ fontSize: '12pt' }} />
+                     <Button clickHandler={this.setShowCategories} label='Show by categories' style={{ fontSize: '12pt', width: 200 }} />
                   </div>
                }
 
