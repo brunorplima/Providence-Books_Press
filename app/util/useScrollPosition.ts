533932fdd06da5a011a091bbe26e5e96f7260f5a
Bruno Lima <brunolima.cad@gmail.com>
2021-05-11 19:37:12 -0600

    added useScrollPosition custom hook

---
+++ app/util/useScrollPosition.ts
@@ -0,0 +1,22 @@
+import React, { useState, useLayoutEffect } from 'react';
+
+const useScrollPosition = () => {
+   const [scrollPosition, setScrollPosition] = useState<number | null>(null);
+
+   useLayoutEffect(() => {
+      setScrollPosition(window.scrollY);
+   }, [])
+
+   useLayoutEffect(() => {
+      const assignScrollPosition = () => {
+         setScrollPosition(window.scrollY);
+      }
+      window.addEventListener('scroll', assignScrollPosition, { once: true });
+
+      return () => window.removeEventListener('scroll', assignScrollPosition);
+   }, [scrollPosition])
+
+   return scrollPosition;
+}
+
+export default useScrollPosition;
