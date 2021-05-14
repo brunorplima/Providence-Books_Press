1bc3bbd6bf22598aa2e8ed04d85ef85cb0a7a544
Bruno Lima <brunolima.cad@gmail.com>
2021-05-14 14:22:20 -0600

    refactored banner to be reused on other pages

--- app/components/elements/banner/Banner.tsx
+++ app/components/elements/banner/Banner.tsx
@@ -3,27 +3,37 @@ import styles from '../../../styles/elements/Banner.module.css'
 import useScrollPosition from '../../../util/useScrollPosition';
 
 interface Props {
-   readonly image: string,
-   readonly title?: string,
-   readonly subtitle?: string
+   readonly image: string;
+   readonly title?: string;
+   readonly subtitle?: string;
+   readonly content?: JSX.Element
 }
 
-const Banner: React.FC<Props> = ({ image, title, subtitle }) => {
-   return (
-      <div className={styles.container} style={image ? { background: `url(${image})`, backgroundRepeat: 'no-repeat' } : {}}>
-         {
-            title ?
-            <div className={styles.title}>
-               <div>{title}</div>
-               {
-                  subtitle &&
-                  <div className={styles.subtitle}>{subtitle}</div>
-               }
-            </div> :
-            <div>
+const Banner: React.FC<Props> = ({ image, title, subtitle, content }) => {
 
-            </div>
-         }
+   if ((title || subtitle) && content) throw new Error('Banner cannot have a title and a content simultaneously.');
+
+   return (
+      <div
+         className={styles.container}
+         style={image ? { background: `url(${image})`, backgroundRepeat: 'no-repeat' } : {}}
+      >
+         <div className={styles.overlay}>
+            {
+               title &&
+               <div className={styles.title}>
+                  <div>{title}</div>
+                  {
+                     subtitle &&
+                     <div className={styles.subtitle}>{subtitle}</div>
+                  }
+               </div>
+            }
+
+            {
+               content && content
+            }
+         </div>
       </div>
    )
 }
