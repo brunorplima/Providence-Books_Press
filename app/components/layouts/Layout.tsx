0c0e22c8ba38e67b40d2965d535580b8e9ca9a76
Bruno Lima <brunolima.cad@gmail.com>
2021-05-17 02:32:00 -0600

    included navbar into layout component

--- app/components/layouts/Layout.tsx
+++ app/components/layouts/Layout.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ClosedStoreMessage from '../elements/closed-store/ClosedStoreMessage'
+import NavbarContainer from '../modules/navbar/NavbarContainer'
 
 const Layout: React.FC = ({ children }) => {
    return (
       <div>
+         <NavbarContainer />
          {children}
          <ClosedStoreMessage />
       </div>
