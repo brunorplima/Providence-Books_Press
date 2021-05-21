334fb1bea6fba99cf726c9cc14184b7c56c21118
Bruno Lima <brunolima.cad@gmail.com>
2021-05-20 22:59:16 -0600

    added query usage to the articles api

--- pages/api/articles/index.ts
+++ pages/api/articles/index.ts
@@ -1,8 +1,28 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { Article } from "../../../app/interfaces-objects/interfaces";
+import ArticlesPage from "../../articles/index";
 // import articles from "./dataCreator";
 import articles from './articles.json'
 
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-   res.status(200).json(articles);
+   const { limit, sorted } = req.query;
+   let finalArticles: Article[] = articles.map(art => {
+      const article: Article = {
+         ...art,
+         datePosted: new Date(art.datePosted)
+      }
+      return article;
+   });
+   
+   if (sorted === 'y') {
+      const comp = new ArticlesPage({articles: [], categories: []}); 
+      finalArticles = comp.getSortedArticles(finalArticles)
+   }
+   if (limit) {
+      res.status(200).json(finalArticles.filter((a, i) => i < parseInt(limit as string)));
+   }
+   else {
+      res.status(200).json(finalArticles);
+   }
 }
