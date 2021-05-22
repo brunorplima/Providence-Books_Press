9f8afc640157541ba846331e0fc64c47b4bba639
Bruno Lima <brunolima.cad@gmail.com>
2021-05-22 16:18:50 -0600

    Added add review button to UserReviews & refactoring

--- app/components/modules/product-details/UserReviews.tsx
+++ app/components/modules/product-details/UserReviews.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import { Review } from '../../../interfaces-objects/interfaces'
-import styles from '../../../styles/product-details/UserReviews.module.css'
-import generalStyles from '../../../styles/product-details/ProductDetails.module.css'
-import UserReview from './UserReview'
+import React from 'react';
+import { Review } from '../../../interfaces-objects/interfaces';
+import styles from '../../../styles/product-details/UserReviews.module.css';
+import generalStyles from '../../../styles/product-details/ProductDetails.module.css';
+import UserReview from './UserReview';
+import Button from '../../elements/button/Button';
 
 interface Props {
-   reviews: Review[]
+   readonly reviews: Review[];
 }
 
 const UserReviews: React.FC<Props> = ({ reviews }) => {
@@ -16,18 +17,32 @@ const UserReviews: React.FC<Props> = ({ reviews }) => {
          <div>
             {
                reviews.length ?
-               reviews.map((review: Review) => {
-                  return <div key={review._id} className={styles.userReviewPLositioner}>
-                     <UserReview review={review} />
-                     <div className={generalStyles.border}></div>
-                  </div>
-               })
-               :
-               <p style={{textAlign: 'center'}}>There are no reviews for this product yet.</p>
+                  reviews.map((review: Review, idx) => {
+                     return <div key={review._id} className={styles.userReviewPLositioner}>
+                        <UserReview review={review} />
+                        {
+                           idx + 1 !== reviews.length &&
+                           <div className={generalStyles.border}></div>
+                        }
+                     </div>
+                  })
+                  :
+                  <p style={{ textAlign: 'center' }}>There are no reviews for this product yet.</p>
             }
+         </div>
+
+         <div className={styles.button}>
+            <Button
+               label="ADD YOUR VOICE"
+               clickHandler={() => { }}
+            />
+         </div>
+
+         <div style={{ display: 'grid', placeItems: 'center' }}>
+            <div className={generalStyles.border}></div>
          </div>
       </div>
    )
 }
 
-export default UserReviews
+export default UserReviews;
