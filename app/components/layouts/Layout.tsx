31a3f3ab4de7436a3599fdbc919c29c7f13fb9ee
Bruno Lima <brunolima.cad@gmail.com>
2021-05-25 04:24:24 -0600

    added Loading comp to Layout

--- app/components/layouts/Layout.tsx
+++ app/components/layouts/Layout.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ClosedStoreMessage from '../elements/closed-store/ClosedStoreMessage'
 import Footer from '../elements/footer/Footer'
+import Loading from '../modules/loading/Loading'
 import NavbarContainer from '../modules/navbar/NavbarContainer'
 
 const Layout: React.FC = ({ children }) => {
    return (
       <div>
          <NavbarContainer />
+         <Loading/>
          {children}
          <ClosedStoreMessage />
          <Footer />
