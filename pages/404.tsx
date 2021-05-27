1cbac4904e8875ec52c6936fe285c1c840b5a027
Bruno Lima <brunolima.cad@gmail.com>
2021-05-27 02:51:18 -0600

    added code to set isLoading redux state to false

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import Button from '../app/components/elements/button/Button';
 import LinkLoading from '../app/components/elements/link-loading/LinkLoading';
+import createLoadingAction from '../app/redux/actions/loadingAction';
 import styles from '../app/styles/404/404.module.css';
 
 const Custom404 = () => {
+   const dispatch = useDispatch();
+
+   useEffect(() => {
+      dispatch(createLoadingAction(false));
+   }, []);
+
    return (
       <div className={styles.container}>
          <div className={styles.image}>
