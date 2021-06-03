17b6e4db28d11e4870ea26dd45915415fa889fa4
Bruno Lima <brunolima.cad@gmail.com>
2021-06-03 00:18:44 -0600

    added max-height and scroll to filter box options

--- app/components/elements/filter-box/FilterBox.tsx
+++ app/components/elements/filter-box/FilterBox.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import CheckboxInput from '../checkbox-input/CheckboxInput'
-import styles from '../../../styles/side-bar/FilterBox.module.css'
+import React from 'react';
+import CheckboxInput from '../checkbox-input/CheckboxInput';
+import styles from '../../../styles/elements/FilterBox.module.css';
 
 interface Props {
-   idIncrement: string,
-   boxTitle: string,
-   optionsList: string[],
-   optionsChecked: string[],
-   setCheckedList: (value: string[]) => void 
+   readonly idIncrement: string;
+   readonly boxTitle: string;
+   readonly optionsList: string[];
+   readonly optionsChecked: string[];
+   readonly setCheckedList: (value: string[]) => void;
 }
 
 const FilterBox: React.FC<Props> = ({ idIncrement, boxTitle, optionsList, optionsChecked, setCheckedList }) => {
@@ -15,23 +15,25 @@ const FilterBox: React.FC<Props> = ({ idIncrement, boxTitle, optionsList, option
    return (
       <div>
          <h4 className={styles.boxTitle}>{boxTitle}</h4>
-         {
-            optionsList.map(((option, idx) => {
-               return (
-                  <div className={styles.option} key={option} style={{paddingLeft: '1rem'}}>
-                     <CheckboxInput
-                        id={`${boxTitle}-${idx}-${option}-${idIncrement}`}
-                        option={option}
-                        optionsChecked={optionsChecked}
-                        setCheckedList={setCheckedList}
-                     />
-                     <label className={styles.label} htmlFor={`${boxTitle}-${idx}-${option}-${idIncrement}`}>{option.toUpperCase()}</label>
-                  </div>
-               )
-            }))
-         }
+         <div className={styles.options}>
+            {
+               optionsList.map(((option, idx) => {
+                  return (
+                     <div className={styles.option} key={option} style={{ paddingLeft: '1rem' }}>
+                        <CheckboxInput
+                           id={`${boxTitle}-${idx}-${option}-${idIncrement}`}
+                           option={option}
+                           optionsChecked={optionsChecked}
+                           setCheckedList={setCheckedList}
+                        />
+                        <label className={styles.label} htmlFor={`${boxTitle}-${idx}-${option}-${idIncrement}`}>{option.toUpperCase()}</label>
+                     </div>
+                  )
+               }))
+            }
+         </div>
       </div>
    )
 }
 
-export default FilterBox
+export default FilterBox;
