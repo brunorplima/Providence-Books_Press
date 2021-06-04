ee534f68b7add9d0f1758086a6d6a4d4b77ac5a8
Bruno Lima <brunolima.cad@gmail.com>
2021-06-03 23:27:48 -0600

    added search feature on /articles

--- pages/articles/index.tsx
+++ pages/articles/index.tsx
@@ -5,8 +5,8 @@ import ArticlesList from '../../app/components/modules/articles/ArticlesList';
 import { Article } from '../../app/interfaces-objects/interfaces';
 import ArticleBanner from '../../app/components/modules/articles/ArticleBanner';
 import Button from '../../app/components/elements/button/Button';
-import { store } from '../../app/redux/store/store';
-import createLoadingAction from '../../app/redux/actions/loadingAction';
+import SearchField from '../../app/components/elements/search-field/SearchField';
+import EmptyResult from '../../app/components/elements/empty-result/EmptyResult';
 
 interface Props {
    readonly articles: Article[],
@@ -15,6 +15,7 @@ interface Props {
 
 interface State {
    showCategories: boolean;
+   search: string;
 }
 
 export class ArticlesPage extends Component<Props, State> {
@@ -23,15 +24,34 @@ export class ArticlesPage extends Component<Props, State> {
       super(props);
 
       this.state = {
-         showCategories: false
+         showCategories: false,
+         search: ''
       }
 
       this.setShowCategories = this.setShowCategories.bind(this);
+      this.setSearch = this.setSearch.bind(this);
    }
 
    getSortedArticles(articles: Article[]): Article[] {
       let sorted = articles.sort((a, b) => new Date(b.datePosted).getTime() - new Date(a.datePosted).getTime())
       return sorted;
+   }
+
+   getArticlesWithSearch(articles: Article[]): Article[] {
+      const { search } = this.state;
+      const regexp = new RegExp(search, 'i');
+      return articles.filter(art => {
+         if (regexp.test(art.title)) return true;
+         if (regexp.test(art.subtitle)) return true;
+         return false;
+      })
+   }
+
+   setSearch(value: string) {
+      const { search } = this.state;
+      if (value !== search) {
+         this.setState({ search: value });
+      }
    }
 
    private setShowCategories() {
@@ -40,8 +60,13 @@ export class ArticlesPage extends Component<Props, State> {
 
    render() {
       const { articles, categories } = this.props;
+      const { search } = this.state;
 
       const firstArticle = this.getSortedArticles(articles)[0];
+      const articlesWithSearch = this.getArticlesWithSearch(articles);
+      const searchMessage = articlesWithSearch.length ?
+         `${articlesWithSearch.length} articles ${articlesWithSearch.length > 1 ? 'match' : 'matches'} your search`
+         : 'There are no matches for your search'
 
       return (
          <div className={styles.container}>
@@ -49,33 +74,61 @@ export class ArticlesPage extends Component<Props, State> {
                <ArticleBanner article={firstArticle} />
             </div>
 
-            <div className={styles.lists}>
-               <ArticlesList
-                  articles={this.getSortedArticles(articles)}
-                  category='All categories'
-                  showFirst={false}
+            <div className={styles.search}>
+               <div>
+                  {
+                     search &&
+                     <div className={styles.searchMessage}>{searchMessage}</div>
+                  }
+               </div>
+               <SearchField
+                  value={search}
+                  changeHandler={this.setSearch}
+                  isGlobalSearch={false}
+                  placeholder='Search'
                />
+            </div>
 
+            <div className={styles.lists}>
                {
-                  !this.state.showCategories &&
-                  <div style={{ marginBottom: '5rem', display: 'flex', justifyContent: 'center' }}>
-                     <Button clickHandler={this.setShowCategories} label='Sort by categories' style={{ fontSize: '12pt', width: 200 }} />
-                  </div>
-               }
-
-               {
-                  this.state.showCategories &&
-                  categories.map(category => {
-                     return (
+                  articlesWithSearch.length ?
+                     <>
                         <ArticlesList
-                           key={category}
-                           articles={articles.filter(article => article.category === category)}
-                           category={category}
-                           showFirst={true}
+                           articles={this.getSortedArticles(articlesWithSearch)}
+                           category='All categories'
+                           showFirst={search ? true : false}
                         />
-                     )
-                  })
+
+                        {
+                           !this.state.showCategories &&
+                           <div style={{ marginBottom: '5rem', display: 'flex', justifyContent: 'center' }}>
+                              <Button clickHandler={this.setShowCategories} label='Sort by categories' style={{ fontSize: '12pt', width: 200 }} />
+                           </div>
+                        }
+
+                        {
+                           this.state.showCategories &&
+                           categories.map(category => {
+                              return (
+                                 <ArticlesList
+                                    key={category}
+                                    articles={this.getArticlesWithSearch(articles.filter(article => article.category === category))}
+                                    category={category}
+                                    showFirst={true}
+                                 />
+                              )
+                           })
+                        }
+                     </>
+                     : null
                }
+
+               {
+                  !articlesWithSearch.length ?
+                     <div style={{ marginTop: '3rem' }}><EmptyResult image='search field' /></div>
+                     : null
+               }
+
             </div>
 
          </div>
