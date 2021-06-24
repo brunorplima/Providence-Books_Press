9b1bc084df08a8c666b35d1ede24c6485888f6a1
Bruno Lima <brunolima.cad@gmail.com>
2021-06-24 03:55:06 -0600

    new component Box

---
+++ app/components/modules/admin-user/Box.tsx
@@ -0,0 +1,33 @@
+import React, { CSSProperties } from 'react';
+import styles from '../../../styles/admin-user/Box.module.css';
+
+interface Props {
+   readonly title?: string;
+   readonly paddingAll?: boolean;
+   readonly paddingVertical?: boolean;
+}
+
+const Box: React.FC<Props> = ({
+   children,
+   title,
+   paddingAll,
+   paddingVertical
+}) => {
+   const containerStyle: CSSProperties = {};
+   if (paddingVertical) containerStyle.padding = '0 0 1rem 0';
+   if (paddingAll) containerStyle.padding = '0 1rem 1rem 1rem';
+
+   const titleStyle: CSSProperties = {};
+   if (!paddingAll) titleStyle.paddingLeft = '1rem'
+   return (
+      <div 
+         className={styles.container}
+         style={containerStyle}
+      >
+         {title && <h3 style={titleStyle}>{title}</h3>}
+         {children}
+      </div>
+   )
+}
+
+export default Box;
