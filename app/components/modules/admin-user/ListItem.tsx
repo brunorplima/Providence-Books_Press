bf95978507770215e4ff1ff5e9373d295cc5f814
Bruno Lima <brunolima.cad@gmail.com>
2021-06-25 02:50:40 -0600

    added actions div and dialog

--- app/components/modules/admin-user/ListItem.tsx
+++ app/components/modules/admin-user/ListItem.tsx
@@ -1,19 +1,56 @@
 import React from 'react';
+import { RiDeleteBinFill, RiFileEditFill } from 'react-icons/ri';
+import { deleteAny } from '../../../firebase/delete';
+import { closeDialog, openDialog } from '../../../redux/actions/openedDialogNameAction';
+import Dialog from '../dialog/Dialog';
+import { CgDetailsMore } from 'react-icons/cg';
 
 interface Props {
+   readonly itemId: string;
+   readonly firestorePath: string;
+   readonly itemType: 'product' | 'article' | 'order';
    readonly isFirstItem: boolean;
 }
 
-const ListItem: React.FC<Props> = ({ children, isFirstItem }) => {
+const ListItem: React.FC<Props> = ({ children, isFirstItem, itemId, firestorePath, itemType }) => {
 
    let className = 'LI-item';
 
    if (isFirstItem) className += ' LI-borderTop';
 
+   const dialogName = 'Delete-item-' + itemId;
+
    return (
-      <div className={className}>
-         {children}
-      </div>
+      <>
+         <div className={className}>
+            {children}
+            <div className='LI-actions'>
+               <div><CgDetailsMore /></div>
+               <div><RiFileEditFill /></div>
+               <div onClick={() => openDialog(dialogName)}>
+                  <RiDeleteBinFill />
+               </div>
+            </div>
+         </div>
+
+         <Dialog
+            name={dialogName}
+            message={`Do you want to permanently delete this ${itemType} from the database?`}
+            buttonsOptions={[
+               {
+                  label: 'Delete',
+                  secondaryStyle: true,
+                  clickHandler: () => deleteAny(firestorePath, itemId)
+               },
+               {
+                  label: 'Cancel',
+                  secondaryStyle: false,
+                  clickHandler: () => closeDialog()
+               }
+            ]}
+            dialogType={'warning'}
+         />
+      </>
    )
 }
 
