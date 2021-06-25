3210fcf1dd191979b5626a4fe7f42d479125592f
Bruno Lima <brunolima.cad@gmail.com>
2021-06-25 03:59:37 -0600

    td

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -65,27 +65,27 @@ export class AdminPage extends Component<{}, State> {
    }
 
    async listenForProducts() {
-      this.productsUnsubscriber = firestore.collection('products').orderBy('name').onSnapshot(snapshot => {
-         const prods: Product[] = [];
-         snapshot.forEach(doc => {
-            prods.push(doc.data() as Product);
-         })
-         this.setProducts(prods);
-      }, error => {
-         window.alert(`${error.name} error occurred: ${error.message}`);
-      })
+      // this.productsUnsubscriber = firestore.collection('products').orderBy('name').onSnapshot(snapshot => {
+      //    const prods: Product[] = [];
+      //    snapshot.forEach(doc => {
+      //       prods.push(doc.data() as Product);
+      //    })
+      //    this.setProducts(prods);
+      // }, error => {
+      //    window.alert(`${error.name} error occurred: ${error.message}`);
+      // })
    }
 
    async listenForArticles() {
-      this.articlesUnsubscriber = firestore.collection('articles').orderBy('title').onSnapshot(snapshot => {
-         const arts: Article[] = [];
-         snapshot.forEach(doc => {
-            arts.push(doc.data() as Article);
-         })
-         this.setArticles(arts);
-      }, error => {
-         window.alert(`${error.name} error occurred: ${error.message}`);
-      })
+      // this.articlesUnsubscriber = firestore.collection('articles').orderBy('title').onSnapshot(snapshot => {
+      //    const arts: Article[] = [];
+      //    snapshot.forEach(doc => {
+      //       arts.push(doc.data() as Article);
+      //    })
+      //    this.setArticles(arts);
+      // }, error => {
+      //    window.alert(`${error.name} error occurred: ${error.message}`);
+      // })
    }
 
    render() {
