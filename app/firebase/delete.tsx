6954a2444787c811e0abbfb0270ef796d4aa4287
Bruno Lima <brunolima.cad@gmail.com>
2021-06-25 02:47:41 -0600

    created function to delete from db with path arg

--- app/firebase/delete.tsx
+++ app/firebase/delete.tsx
@@ -1,11 +1,24 @@
 import { firestore } from "./firebase"
 
+export const deleteAny = async (path: string, id: string) => {
+   path += path.substring(path.length - 1) === '/' ? '' : '/';
+   try {
+      const docRef = await firestore.collection(path).where('_id', '==', id).get();
+      let firestoreId: string;
+      docRef.forEach(doc => firestoreId = doc.id);
+      return await firestore.doc(path + firestoreId).delete();
+   }
+   catch (error) {
+      alert('Sorry, the following occurred: ' + error);
+   }
+}
+
 export const deleteProduct = async (id: string) => {
    try {
       const docRef = await firestore.collection('products').where('_id', '==', id).get();
       let firestoreId: string;
       docRef.forEach(doc => firestoreId = doc.id);
-      await firestore.doc(`products/${firestoreId}`).delete();
+      return await firestore.doc(`products/${firestoreId}`).delete();
    }
    catch(error) {
       alert('Sorry, the following occurred: ' + error);
