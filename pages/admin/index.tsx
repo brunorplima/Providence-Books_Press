7e5b3e5b29c7d7359d18c0d9e1a566ecc9794ffe
Bruno Lima <brunolima.cad@gmail.com>
2021-06-25 03:11:15 -0600

    added articles section and data

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import AdminArticles from '../../app/components/modules/admin-user/AdminArticles';
 import AdminDashboard from '../../app/components/modules/admin-user/AdminDashboard';
 import AdminProducts from '../../app/components/modules/admin-user/AdminProducts';
 import Section from '../../app/components/modules/admin-user/Section';
 import Sidebar from '../../app/components/modules/admin-user/Sidebar';
 import { firestore } from '../../app/firebase/firebase';
+import { Article } from '../../app/interfaces-objects/interfaces';
 import Product from '../../app/interfaces-objects/Product';
 import styles from '../../app/styles/admin-user/Admin.module.css';
 
@@ -12,29 +14,35 @@ const sections = ['Dashboard', 'Products', 'Articles', 'Users', 'Orders', 'Conte
 interface State {
    readonly currentSection: string;
    readonly products: Product[];
+   readonly articles: Article[];
 }
 
 export class AdminPage extends Component<{}, State> {
 
-   productsUnsubscriber: () => void;
+   productsUnsubscriber = () => {};
+   articlesUnsubscriber = () => {};
 
    constructor(props) {
       super(props);
       this.state = {
          currentSection: sections[0],
-         products: []
+         products: [],
+         articles: [],
       }
 
       this.setCurrentSection = this.setCurrentSection.bind(this);
       this.setProducts = this.setProducts.bind(this);
    }
 
-   componentDidMount() {
-      this.listenForProducts();
+   componentDidUpdate() {
+      const { currentSection, products, articles } = this.state;
+      if (currentSection === sections[1] && !products.length) this.listenForProducts();
+      if (currentSection === sections[2] && !articles.length) this.listenForArticles();
    }
 
    componentWillUnmount() {
       this.productsUnsubscriber();
+      this.articlesUnsubscriber();
    }
 
    setCurrentSection(index: number) {
@@ -50,6 +58,12 @@ export class AdminPage extends Component<{}, State> {
       }
    }
 
+   setArticles(articles: Article[]) {
+      if (JSON.stringify(articles) !== JSON.stringify(this.state.articles)) {
+         this.setState({ articles });
+      }
+   }
+
    listenForProducts() {
       this.productsUnsubscriber = firestore.collection('products').orderBy('name').onSnapshot(snapshot => {
          const prods: Product[] = [];
@@ -58,12 +72,24 @@ export class AdminPage extends Component<{}, State> {
          })
          this.setProducts(prods);
       }, error => {
-         window.alert(`A ${error.name} error occurred: ${error.message}`)
+         window.alert(`${error.name} error occurred: ${error.message}`);
+      })
+   }
+
+   listenForArticles() {
+      this.articlesUnsubscriber = firestore.collection('articles').orderBy('title').onSnapshot(snapshot => {
+         const arts: Article[] = [];
+         snapshot.forEach(doc => {
+            arts.push(doc.data() as Article);
+         })
+         this.setArticles(arts);
+      }, error => {
+         window.alert(`${error.name} error occurred: ${error.message}`);
       })
    }
 
    render() {
-      const { currentSection, products } = this.state;
+      const { currentSection, products, articles } = this.state;
       return (
          <div className={styles.container}>
             <Sidebar
@@ -92,42 +118,45 @@ export class AdminPage extends Component<{}, State> {
             {
                currentSection === sections[2] &&
                <Section title={currentSection} tabs>
-                  
+                  <AdminArticles
+                     list={articles}
+                     tabs={['Overview', 'Add', 'Update']}
+                  />
                </Section>
             }
 
             {
                currentSection === sections[3] &&
                <Section title={currentSection}>
-                  
+
                </Section>
             }
 
             {
                currentSection === sections[4] &&
                <Section title={currentSection}>
-                  
+
                </Section>
             }
 
             {
                currentSection === sections[5] &&
                <Section title={currentSection}>
-                  
+
                </Section>
             }
 
             {
                currentSection === sections[6] &&
                <Section title={currentSection}>
-                  
+
                </Section>
             }
 
             {
                currentSection === sections[7] &&
                <Section title={currentSection}>
-                  
+
                </Section>
             }
          </div>
