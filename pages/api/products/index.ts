90fdbca82934652924f09af11a7cf4009b6dd51a
Bruno Lima <brunolima.cad@gmail.com>
2021-06-27 02:24:07 -0600

    td

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -1,9 +1,14 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import { firestore } from '../../../app/firebase/firebase';
 import Product from '../../../app/interfaces-objects/Product';
 import productsJSON from './products.json'
 // import products from './dataCreator'
 
 export default async (req: NextApiRequest, res: NextApiResponse<Product[]>) => {
-   const products = productsJSON as Product[]
-   res.status(200).json(products)
+   // const products = productsJSON as Product[]
+   // res.status(200).json(products)
+   const products: Product[] = [];
+   const prodRef = await firestore.collection('products').get();
+   prodRef.forEach(doc => products.push(doc.data() as Product))
+   res.status(200).json(products);
 }
