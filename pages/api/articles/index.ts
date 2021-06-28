701f216e951c7365d49e8a202b595335c64c5afd
Bruno Lima <brunolima.cad@gmail.com>
2021-06-28 00:01:26 -0600

    now API endpoints connect with firestore

--- pages/api/articles/index.ts
+++ pages/api/articles/index.ts
@@ -1,28 +1,42 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import firebase, { firestore } from "../../../app/firebase/firebase";
 import { Article } from "../../../app/interfaces-objects/interfaces";
 import ArticlesPage from "../../articles/index";
 // import articles from "./dataCreator";
 import articles from './articles.json'
 
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
+export default async (req: NextApiRequest, res: NextApiResponse) => {
    const { limit, sorted } = req.query;
-   let finalArticles: Article[] = articles.map(art => {
-      const article: Article = {
-         ...art,
-         datePosted: new Date(art.datePosted)
-      }
-      return article;
-   });
+   // let finalArticles: Article[] = [];
+   let articles: Article[] = [];
+   let articlesRef: 
+      firebase.firestore.CollectionReference<firebase.firestore.DocumentData>
+      | firebase.firestore.Query<firebase.firestore.DocumentData>
+      | firebase.firestore.QuerySnapshot<firebase.firestore.DocumentData>
+      | null = null;
    
+   articlesRef = firestore.collection('articles');
    if (sorted === 'y') {
-      const comp = new ArticlesPage({articles: [], categories: []}); 
-      finalArticles = comp.getSortedArticles(finalArticles)
+      // const comp = new ArticlesPage({articles: [], categories: []}); 
+      // finalArticles = comp.getSortedArticles(finalArticles)
+      articlesRef = articlesRef.orderBy('title');
    }
    if (limit) {
-      res.status(200).json(finalArticles.filter((a, i) => i < parseInt(limit as string)));
+      // res.status(200).json(finalArticles.filter((a, i) => i < parseInt(limit as string)));
+      articlesRef = articlesRef.limit(Number(limit));
    }
-   else {
-      res.status(200).json(finalArticles);
-   }
+   // else {
+   //    res.status(200).json(finalArticles);
+   // }
+   articlesRef = await articlesRef.get();
+   articlesRef.forEach(doc => {
+      const article: Article = {
+         ...doc.data() as Article,
+         datePosted: new Date(doc.data().datePosted)
+      }
+      articles.push(article);
+   })
+
+   res.status(200).json(articles);
 }
--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -1,9 +1,22 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import firebase, { firestore } from '../../../app/firebase/firebase';
 import Product from '../../../app/interfaces-objects/Product';
-import productsJSON from './products.json'
+// import productsJSON from './products.json'
 // import products from './dataCreator'
 
 export default async (req: NextApiRequest, res: NextApiResponse<Product[]>) => {
-   const products = productsJSON as Product[]
-   res.status(200).json(products)
+   const { sorted } = req.query;
+   // const products = productsJSON as Product[]
+   // res.status(200).json(products)
+   const products: Product[] = [];
+   let productsRef: 
+      firebase.firestore.CollectionReference<firebase.firestore.DocumentData>
+      | firebase.firestore.Query<firebase.firestore.DocumentData>
+      | firebase.firestore.QuerySnapshot<firebase.firestore.DocumentData>
+      | null = null;
+   productsRef = firestore.collection('products');
+   if (sorted === 'y') productsRef = productsRef.orderBy('name');
+   productsRef = await productsRef.get();
+   productsRef.forEach(doc => products.push(doc.data() as Product));
+   res.status(200).json(products);
 }
