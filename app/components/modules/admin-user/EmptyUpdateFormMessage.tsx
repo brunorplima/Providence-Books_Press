2397dd5396e640076b429c1ad1b6ae52d99f6498
Bruno Lima <brunolima.cad@gmail.com>
2021-07-15 23:41:36 -0600

    new component EmptyUpdateFormMessage

---
+++ app/components/modules/admin-user/EmptyUpdateFormMessage.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { AiFillInfoCircle } from 'react-icons/ai'
+import { RiFileEditFill } from 'react-icons/ri';
+import styles from '../../../styles/admin-user/EmptyUpdateFormMessage.module.css'
+
+interface Props {
+   readonly messageType: 'product' | 'article' | 'order'
+}
+
+const EmptyUpdateFormMessage: React.FC<Props> = ({ messageType }) => {
+   return (
+      <div className={styles.updateMessage}>
+         <AiFillInfoCircle size={30} />
+         To update {messageType === 'product' ? 'a' : 'an'} {messageType} select it ({<RiFileEditFill />}) on the "Overview" tab
+      </div>
+   )
+}
+
+export default EmptyUpdateFormMessage
