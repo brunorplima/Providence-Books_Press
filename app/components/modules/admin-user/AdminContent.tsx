c953b987e4f75b8bf72b3c4dc9a17d317de8b27f
Bruno Lima <brunolima.cad@gmail.com>
2021-07-18 03:05:35 -0600

    new component AdminContent

---
+++ app/components/modules/admin-user/AdminContent.tsx
@@ -0,0 +1,120 @@
+import React, { useEffect, useState } from 'react'
+import { firestore } from '../../../firebase/firebase'
+import Loading from '../loading/Loading'
+import Box from './Box'
+import styles from '../../../styles/admin-user/AdminContent.module.css'
+import AdminContentControllers from './AdminContentControllers'
+import clsx from 'clsx'
+import { updateContent } from '../../../firebase/update'
+
+const AdminContent = () => {
+   const [aboutUsText, setAboutUsText] = useState('')
+   const [editAboutUsText, setEditAboutUsText] = useState('')
+   const [biblePassage, setBiblePassage] = useState('')
+   const [editBiblePassage, setEditBiblePassage] = useState('')
+   const [isTextEditMode, setIsTextEditMode] = useState(false)
+   const [isPassageEditMode, setIsPassageEditMode] = useState(false)
+   const [isLoading, setIsLoading] = useState(true)
+
+   useEffect(() => {
+      firestore.doc('content/about-us').get()
+         .then(doc => {
+            setAboutUsText(doc.data().mainText)
+            setBiblePassage(doc.data().biblicalText)
+            setIsLoading(false)
+         })
+         .catch(error => {
+            console.error(error)
+         })
+   }, [])
+
+   function openTextEditMode() {
+      if (!isLoading) {
+         setIsTextEditMode(true)
+         setEditAboutUsText(aboutUsText)
+      }
+   }
+
+   function openPassageEditMode() {
+      if (!isLoading) {
+         setIsPassageEditMode(true)
+         setEditBiblePassage(biblePassage)
+      }
+   }
+
+   async function saveText() {
+      await updateContent('SET_MAIN_TEXT', editAboutUsText)
+      setAboutUsText(editAboutUsText)
+      setIsTextEditMode(false)
+   }
+
+   async function savePassage() {
+      await updateContent('SET_PASSAGE', editBiblePassage)
+      setBiblePassage(editBiblePassage)
+      setIsPassageEditMode(false)
+   }
+
+   return (
+      <div>
+         <Box paddingAll title="MANAGE WEBSITE'S CONTENT">
+            <h2>About page</h2>
+            <div>
+               <div className={styles.contentContainer} style={{ marginBottom: 30 }}>
+                  <div className={styles.contentLabel}>Main text:</div>
+                  <div className={styles.content}>
+                     {
+
+                        !isLoading ?
+                           isTextEditMode ?
+                              <textarea
+                                 className={clsx(styles.textTextarea, styles.textarea)}
+                                 value={editAboutUsText}
+                                 onChange={e => setEditAboutUsText(e.currentTarget.value)}
+                              />
+                              : <pre>{aboutUsText}</pre>
+                           : <Loading localIsLoading size={4} />
+                     }
+
+                     <div className={styles.controllers}>
+                        <AdminContentControllers
+                           isEditMode={isTextEditMode}
+                           setIsEditMode={setIsTextEditMode}
+                           openEditMode={openTextEditMode}
+                           save={saveText}
+                        />
+                     </div>
+                  </div>
+               </div>
+
+               <div className={styles.contentContainer}>
+                  <div className={styles.contentLabel}>Bible passage:</div>
+                  <div className={styles.content}>
+                     {
+                        !isLoading ?
+                           isPassageEditMode ?
+                              <textarea
+                                 className={clsx(styles.passageTextarea, styles.textarea)}
+                                 value={editBiblePassage}
+                                 onChange={e => setEditBiblePassage(e.currentTarget.value)}
+                              />
+                              : <pre>{biblePassage}</pre>
+                           : <Loading localIsLoading size={4} />
+                     }
+
+                     <div className={styles.controllers}>
+                        <AdminContentControllers
+                           isEditMode={isPassageEditMode}
+                           setIsEditMode={setIsPassageEditMode}
+                           openEditMode={openPassageEditMode}
+                           save={savePassage}
+                        />
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </Box>
+      </div>
+   )
+}
+
+export default AdminContent
