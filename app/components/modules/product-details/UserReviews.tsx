27603b2b8f4973258be439edf410d6505560391e
Bruno Lima <brunolima.cad@gmail.com>
2021-08-09 21:28:37 -0600

    fetch product from redux, render add review modal

--- app/components/modules/product-details/UserReviews.tsx
+++ app/components/modules/product-details/UserReviews.tsx
@@ -5,19 +5,41 @@ import generalStyles from '../../../styles/product-details/ProductDetails.module
 import UserReview from './UserReview';
 import Button from '../../elements/button/Button';
 import ReviewsModal from './ReviewsModal';
+import AddReviewModal from './AddReviewModal';
+import { addReview } from '../../../firebase/add';
 
 interface Props {
    readonly reviews: Review[];
+   readonly productId: string
 }
 
-const UserReviews: React.FC<Props> = ({ reviews }) => {
-   const [showModal, setShowModal] = useState<boolean>(false);
+const UserReviews: React.FC<Props> = ({ reviews, productId }) => {
+   const [showReviewsModal, setShowReviewsModal] = useState(false);
+   const [showAddReviewModal, setShowAddReviewModal] = useState(false);
+   const [score, setScore] = useState(0);
+   const [heading, setHeading] = useState('');
+   const [textBody, setTextBody] = useState('');
    const shortReviewsList = reviews.slice(0, 3);
 
    function backgroundCloseModal(e?: React.SyntheticEvent<HTMLDivElement>) {
       if (e.currentTarget === e.target) {
-         setShowModal(!showModal)
+         setShowReviewsModal(false)
+         setShowAddReviewModal(false)
       }
+   }
+
+   function postReview() {
+      const review: Review = {
+         _id: '98142',
+         _userId: '742834',
+         dateTime: new Date(Date.now()).toString(),
+         text: textBody,
+         userName: 'Mark Junsten',
+         _productId: productId,
+         score,
+         heading
+      }
+      addReview(review, productId)
    }
 
    return (
@@ -47,14 +69,14 @@ const UserReviews: React.FC<Props> = ({ reviews }) => {
                <div style={{ marginRight: '1rem' }}>
                   <Button
                      label='SEE ALL REVIEWS'
-                     clickHandler={() => setShowModal(true)}
+                     clickHandler={() => setShowReviewsModal(true)}
                      style={{width: 145}}
                   />
                </div>
             }
             <Button
                label='ADD YOUR VOICE'
-               clickHandler={() => { }}
+               clickHandler={() => setShowAddReviewModal(true)}
                style={{width: 145}}
             />
          </div>
@@ -65,9 +87,22 @@ const UserReviews: React.FC<Props> = ({ reviews }) => {
 
          <ReviewsModal
             reviews={reviews}
-            showModal={showModal}
+            showModal={showReviewsModal}
             backgroundCloseModal={backgroundCloseModal}
-            closeModal={() => setShowModal(false)}
+            closeModal={() => setShowReviewsModal(false)}
+         />
+
+         <AddReviewModal
+            showModal={showAddReviewModal}
+            closeModal={() => setShowAddReviewModal(false)}
+            backgroundCloseModal={backgroundCloseModal}
+            onSubmit={postReview}
+            score={score}
+            setScore={score => setScore(score)}
+            heading={heading}
+            setHeading={setHeading}
+            textBody={textBody}
+            setTextBody={setTextBody}
          />
       </div>
    )
