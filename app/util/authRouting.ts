83801d2f1c4e7dc3a158282bc64c8566cadb4de1
Bruno Lima <brunolima.cad@gmail.com>
2021-09-04 19:16:19 -0600

    now unverified users cannot login

--- app/util/authRouting.ts
+++ app/util/authRouting.ts
@@ -4,16 +4,21 @@ import firebase from '../firebase/firebase'
 export const redirectLoggedUser = (user: firebase.User) => {
    const router = useRouter()
    if (user) {
-      if (user.uid === process.env.NEXT_PUBLIC_ADMIN_UID) router.replace('/admin')
-      else router.replace('/account')
-      return true
+      if (user.uid === process.env.NEXT_PUBLIC_ADMIN_UID) {
+         router.replace('/admin')
+         return true
+      }
+      if (user.emailVerified) {
+         router.replace('/account')
+         return true
+      }
    }
    return false
 }
 
 export const redirectUnauthorizedUser = (user: firebase.User) => {
    const router = useRouter()
-   if (!user) {
+   if (!user || !user?.emailVerified) {
       router.replace('/sign-in')
       return true
    }
