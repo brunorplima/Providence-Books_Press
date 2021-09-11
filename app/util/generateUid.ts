4c290c44d98a7041ed7d5c06c24f68f07562b5bb
Bruno Lima <brunolima.cad@gmail.com>
2021-09-11 04:13:11 -0600

    added a generic id generator function

--- app/util/generateUid.ts
+++ app/util/generateUid.ts
@@ -10,3 +10,17 @@ export const generateProductID = (existingIds = store.getState().products.map(pr
    }
    return random.toString()
 }
+
+export const generateID = (entity: 'product' | 'article' = 'article') => {
+   const getRandom = () => Math.ceil(Math.random() * (999999 - 100000) + 100000)
+   let random = getRandom()
+   let existingIds: string[] = []
+   if (entity === 'product')
+      existingIds = store.getState().products.map(product => product._id)
+   else
+      existingIds = store.getState().articles.map(article => article._id)
+   while (existingIds.includes(String(random)) || random < 100000) {
+      random = getRandom()
+   }
+   return random.toString()
+}
