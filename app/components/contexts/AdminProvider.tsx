4d623b24b569e58d61d052e5cac918a50454903d
Bruno Lima <brunolima.cad@gmail.com>
2021-09-18 03:27:27 -0600

    new featured products manager

--- app/components/contexts/AdminProvider.tsx
+++ app/components/contexts/AdminProvider.tsx
@@ -6,35 +6,56 @@ import { useAuth } from './AuthProvider'
 interface AdminContext {
    readonly orders: Order[]
    readonly listenForOrders: () => void
+   readonly featuredProductIds: string[]
+   readonly listenForFPIds: () => void
 }
 
 export const adminContext = createContext<AdminContext>({
    orders: [],
-   listenForOrders: () => {}
+   listenForOrders: () => { },
+   featuredProductIds: [],
+   listenForFPIds: () => { }
 })
 
 export const useAdminContext = () => useContext(adminContext)
 
 const AdminProvider: React.FC = ({ children }) => {
    const [orders, setOrders] = useState<Order[]>(null)
+   const [featuredProductIds, setFeaturedProductIds] = useState<string[]>([])
    const { providenceUser } = useAuth()
    const ordersUnsubscribe = useRef<() => void>()
+   const fpUnsubscribe = useRef<() => void>()
 
    useEffect(() => {
-      return ordersUnsubscribe.current
+      return () => {
+         if (ordersUnsubscribe.current) ordersUnsubscribe.current()
+         if (fpUnsubscribe.current) fpUnsubscribe.current()
+      }
    }, [])
 
    function listenForOrders() {
-      ordersUnsubscribe.current = firestore.collection('orders').onSnapshot(snapshot => {
-         const ords: Order[] = []
-         snapshot.forEach(doc => ords.push(doc.data() as Order))
-         setOrders(ords)
-      })
+      if (!ordersUnsubscribe.current) {
+         ordersUnsubscribe.current = firestore.collection('orders').onSnapshot(snapshot => {
+            const ords: Order[] = []
+            snapshot.forEach(doc => ords.push(doc.data() as Order))
+            setOrders(ords)
+         })
+      }
+   }
+
+   function listenForFPIds() {
+      if (!fpUnsubscribe.current) {
+         fpUnsubscribe.current = firestore.collection('featured-products').doc('ids').onSnapshot(snapshot => {
+            setFeaturedProductIds(snapshot.data().ids as string[])
+         })
+      }
    }
 
    const initialValue: AdminContext = {
       orders,
-      listenForOrders
+      listenForOrders,
+      featuredProductIds,
+      listenForFPIds
    }
 
    if (!providenceUser || providenceUser.role === 'user') return <>{children}</>
